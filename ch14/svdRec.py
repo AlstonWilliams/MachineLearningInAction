1cda49e6ef8b29cea9538b83d8b4dfc8063d2663
AlstonWilliams <shuyue.pang@hypers.com>
2019-05-20 10:06:11 +0800

    Finish Chapter14

---
+++ ch14/svdRec.py
@@ -0,0 +1,117 @@
+# -*- coding: UTF-8 -*-
+
+from numpy import *
+from numpy import linalg as la
+
+
+def loadExData():
+    return [[0, 0, 0, 2, 2],
+            [0, 0, 0, 3, 3],
+            [0, 0, 0, 1, 1],
+            [1, 1, 1, 0, 0],
+            [2, 2, 2, 0, 0],
+            [5, 5, 5, 0, 0],
+            [1, 1, 1, 0, 0]]
+
+
+def loadExData2():
+    return [[0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 5],
+            [0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 3],
+            [0, 0, 0, 0, 4, 0, 0, 1, 0, 4, 0],
+            [3, 3, 4, 0, 0, 0, 0, 2, 2, 0, 0],
+            [5, 4, 5, 0, 0, 0, 0, 5, 5, 0, 0],
+            [0, 0, 0, 0, 5, 0, 1, 0, 0, 5, 0],
+            [4, 3, 4, 0, 0, 0, 0, 5, 5, 0, 1],
+            [0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 4],
+            [0, 0, 0, 2, 0, 2, 5, 0, 0, 1, 2],
+            [0, 0, 0, 0, 5, 0, 0, 0, 0, 4, 0],
+            [1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0]]
+
+# 计算两个向量的欧式距离
+def ecludSim(inA,inB):
+    return 1.0/(1.0 + la.norm(inA - inB))
+
+# 计算两个向量的皮儿逊相关系数
+def pearsSim(inA,inB):
+    if len(inA) < 3 : return 1.0
+    return 0.5+0.5*corrcoef(inA, inB, rowvar = 0)[0][1]
+
+# 计算两个向量的余弦相似度
+def cosSim(inA,inB):
+    num = float(inA.T*inB)
+    denom = la.norm(inA)*la.norm(inB)
+    return 0.5+0.5*(num/denom)
+
+# 好好了解一下特征值分解，来理解下面的过程
+def svdEst(dataMat, user, simMeas, item):
+    n = shape(dataMat)[1]
+    simTotal = 0.0; ratSimTotal = 0.0
+    U,Sigma,VT = la.svd(dataMat)
+    Sig4 = mat(eye(4)*Sigma[:4]) # 只取前三个特征，因为它们的能量和已经大于全部能量和的90%
+    xformedItems = dataMat.T * U[:,:4] * Sig4.I  # 构建转换后的新的数据集。为什么用dataMat，而不是U * Sig4 * VT?
+    for j in range(n):
+        userRating = dataMat[user,j]
+        if userRating == 0 or j==item: continue
+        similarity = simMeas(xformedItems[item,:].T,\
+                             xformedItems[j,:].T)
+        print('the %d and %d similarity is: %f' % (item, j, similarity))
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0: return 0
+    else: return ratSimTotal/simTotal
+
+# 计算用户对物品的估计评分值，基于物品相似度
+# 需要进行大量计算，不可能做到实时处理
+# simMeas是相似度计算方法
+# item是待预测物品
+def standEst(dataMat, user, simMeas, item):
+    n = shape(dataMat)[1]
+    simTotal = 0.0; ratSimTotal = 0.0
+    for j in range(n):
+        userRating = dataMat[user,j] # 找到user对j这个物品的评分
+        if userRating == 0: continue # 没评过分，则找下一个物品
+        overLap = nonzero(logical_and(dataMat[:,item].A>0, \
+                                      dataMat[:,j].A>0))[0] # 计算物品j和item是否有重合的部分
+        if len(overLap) == 0: similarity = 0 # 如果一点都没有重合，则物品j和item肯定是不相似的
+        else: similarity = simMeas(dataMat[overLap,item], \
+                                   dataMat[overLap,j]) # 基于重合的部分计算相似度。这样子相似度不总是会为1么？
+        print('the %d and %d similarity is: %f' % (item, j, similarity))
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0: return 0
+    else: return ratSimTotal/simTotal
+
+def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
+    unratedItems = nonzero(dataMat[user,:].A==0)[1] # 找到用户为评分的物品
+    if len(unratedItems) == 0: return 'you rated everything'
+    itemScores = []
+    for item in unratedItems:
+        estimatedScore = estMethod(dataMat, user, simMeas, item) # 计算用户对每个物品的估计评分值
+        itemScores.append((item, estimatedScore))
+    return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[:N] # 返回估计评分最高的N个物品
+
+def printMat(inMat, thresh=0.8):
+    for i in range(32):
+        for k in range(32):
+            if float(inMat[i,k]) > thresh:
+                print(1, end=''),
+            else: print(0, end=''),
+        print('')
+
+def imgCompress(numSV=3, thresh=0.8):
+    myl = []
+    for line in open('0_5.txt').readlines():
+        newRow = []
+        for i in range(32):
+            newRow.append(int(line[i]))
+        myl.append(newRow)
+    myMat = mat(myl)
+    print("****original matrix******")
+    printMat(myMat, thresh)
+    U,Sigma,VT = la.svd(myMat)
+    SigRecon = mat(zeros((numSV, numSV)))
+    for k in range(numSV):#construct diagonal matrix from vector
+        SigRecon[k,k] = Sigma[k]
+    reconMat = U[:,:numSV]*SigRecon*VT[:numSV,:]
+    print("****reconstructed matrix using %d singular values******" % numSV)
+    printMat(reconMat, thresh)
